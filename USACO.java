ae7b788fc3665e8461f32e3d2cf837ca683d212d
joycevw02 <jwang01@stuy.edu>
2019-03-06 14:33:26 -0500

    Initialize the field

--- USACO.java
+++ USACO.java
@@ -1,14 +1,27 @@
 import java.io.*;
 import java.util.*;
 public class USACO{
-  private int row;
-  private int col;
-  private int elevation;
-  p
+
   public static int bronze(String filename){
     try{
+      int row = 0;
+      int col = 0;
+      int coltemp = 0;
+      int elevation = 0;
+      int[][] field;
       File f = new File(filename);
-      Scanner s = new Scanner(f);
+      Scanner count = new Scanner(f);
+      while (count.hasNextLine()){
+        row ++;
+        String temp = count.nextLine();
+        coltemp = 0;
+        for (int i = 0; i < temp.length(); i ++){
+          coltemp ++;
+        }
+        if (coltemp > col){
+          col = coltemp;
+        }
+      }
     }
     catch (FileNotFoundException e){}
   }
