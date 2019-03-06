64c25a2c8969c7bc0bdce5989b65a7f4c7534429
Joyce Wang <jwang01@stuy.edu>
2019-03-05 21:41:51 -0500

    Filled in read file part

--- USACO.java
+++ USACO.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.util.*;
 public class USACO{
   public static int bronze(String filename){
-
+    try{
+      File f = new File(filename);
+      Scanner s = new Scanner(f);
+    }
+    catch (FileNotFoundException e){}
   }
 }
