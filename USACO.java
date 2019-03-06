e29767edc58d76038b98c34e0e7a4a55c9688fa4
joycevw02 <jwang01@stuy.edu>
2019-03-06 14:49:27 -0500

    After further examination of the instructions, found a shorter way to initialize the field and the instruction. Still have to figure out what E does

--- USACO.java
+++ USACO.java
@@ -4,24 +4,21 @@ public class USACO{
 
   public static int bronze(String filename){
     try{
-      int row = 0;
-      int col = 0;
-      int coltemp = 0;
+      int row = 0; //To initialize the field
+      int col = 0; //To initialize the field
+      int inforow = 0; //To initialize the array storing the instructions
       int elevation = 0;
       int[][] field;
+      int[][] info;
       File f = new File(filename);
-      Scanner count = new Scanner(f);
-      while (count.hasNextLine()){
-        row ++;
-        String temp = count.nextLine();
-        coltemp = 0;
-        for (int i = 0; i < temp.length(); i ++){
-          coltemp ++;
-        }
-        if (coltemp > col){
-          col = coltemp;
-        }
-      }
+      Scanner s = new Scanner(f);
+      String temp = s.nextLine();
+      row = Integer.parseInt(temp.substring(0,1));
+      col = Integer.parseInt(temp.substring(1,2));
+      inforow = Integer.parseInt(temp.substring(3,4));
+      field = new int[row][col];
+      info = new int[inforow][3];
+
     }
     catch (FileNotFoundException e){}
   }
