69190742d00325c98684f93331c66fcb50c61b8c
Joyce Wang <jwang01@stuy.edu>
2019-03-05 21:48:05 -0500

    Added ZIP files

--- USACO.java
+++ USACO.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.*;
 public class USACO{
+  private int row;
+  private int col;
+  private int elevation;
+  p
   public static int bronze(String filename){
     try{
       File f = new File(filename);
