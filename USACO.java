f9bb7ec0763ec1c5e93bbc426b4a7dbd547e4f1b
Joyce Wang <jwang01@stuy.edu>
2019-03-07 20:36:59 -0500

    Finished stomp, needs testing

--- USACO.java
+++ USACO.java
@@ -8,9 +8,10 @@ public class USACO{
       int col = 0; //To initialize the field
       int inforow = 0; //To initialize the array storing the instructions
       int elevation = 0;
-      int[][] field;
+      String[][] field;
       int[][] info;
 
+      //Basic info from the text file
       File f = new File(filename);
       Scanner s = new Scanner(f);
       String temp = s.nextLine();
@@ -18,7 +19,7 @@ public class USACO{
       row = Integer.parseInt(test[0]);
       col = Integer.parseInt(test[1]);
       inforow = Integer.parseInt(test[2]);
-      field = new int[row][col];
+      field = new String[row][col];
       info = new int[inforow][3];
 
       int r = 0;
@@ -26,13 +27,16 @@ public class USACO{
         String line = s.nextLine();
         //System.out.println(line);
         String[] lineinfo = line.split(" ");
+        //If r < row (meaning that it is in the field's data), add the data into
+        //the int array
         if (r < row){
           for (int c = 0; c < col; c ++){
-            field[r][c] = Integer.parseInt(lineinfo[c]);
+            field[r][c] = lineinfo[c];
             //System.out.println(field);
           }
           r ++;
         }
+        //If not, it would be in the instructions
         else{
           for (int c = 0; c < 3; c ++){
             info[r][c] = Integer.parseInt(lineinfo[c]);
@@ -44,12 +48,52 @@ public class USACO{
       s.close();
     }
     catch (FileNotFoundException e){}
-      return -1;
+    return -1;
   }
 
-  private static void stomp(int row, int col, int depth){
+  private static void stomp(int row, int col, int depth, String[][] field){
 
+    //Loop through to find the highest area in the the map (within the square)
+    int highrow = 0;
+    int highcol = 0;
+    int high = 0;
+    for (int r = row; r < 3; r ++){
+      for (int c = col; c < 3; c ++){
+        if (Integer.parseInt(field[r][c]) > high){
+          highrow = r;
+          highcol = c;
+          high = Integer.parseInt(field[r][c]);
+        }
+      }
+    }
 
+    //Set temp to the highest because...too lazy to type repeatedly
+    int temp = Integer.parseInt(field[highrow][highcol]);
+    //If temp minus the depth is greater than 0, set the value at the field to
+    //the value minus the depth. Then, loop through the square and if any value
+    //is greater than the target (highest value - depth), then change it to the
+    //target value
+    if (temp - depth > 0){
+      field[highrow][highcol] = temp - depth + "";
+      int target = temp - depth;
+      for (int r = row; r < 3; r ++){
+        for (int c = col; c < 3; c ++){
+          if (Integer.parseInt(field[r][c]) > target){
+            field[r][c] = target + "";
+          }
+        }
+      }
+    }
+    //Else, loop through the square and change them all to --, because none of
+    //the values should be less than 0.
+    else{
+      field[highrow][highcol] = "--";
+      for (int r = row; r < 3; r ++){
+        for (int c = col; c < 3; c ++){
+          field[r][c] = "--";
+        }
+      }
+    }
   }
 
   public static void main(String[] args){
