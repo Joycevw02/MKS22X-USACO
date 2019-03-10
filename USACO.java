afbcce607d573e75b90bb96cf78245f6dacee79d
Joyce Wang <jwang01@stuy.edu>
2019-03-10 19:50:56 -0400

    Empty helper function for the silver

--- USACO.java
+++ USACO.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
 public class USACO{
-  private static int row = 0; //To initialize the field
-  private static int col = 0; //To initialize the field
-  private static int[][] field;
 
   public static int bronze(String filename){
     try{
+      int row = 0; //To initialize the field
+      int col = 0; //To initialize the field
+      int[][] field;
       int inforow = 0; //To initialize the array storing the instructions
       int elevation = 0;
       int[][] info;
@@ -93,21 +93,69 @@ public class USACO{
 
   }
 
-  public String toString(){
-    String ans = "";
-    for (int r = 0; r < row; r ++){
-      for (int c = 0; c < col; c ++){
-        ans = ans + field[r][c] + " ";
+
+  public static int silver(String filename){
+    try{
+      int[][] field;
+      int[][] moves = {{1,0},{-1,0},{0,-1},{0,1}};
+      int row = 0; //To initialize the field
+      int col = 0; //To initialize the field
+      int timer = 0; //Timer
+      int r1 = 0; //Starting row
+      int r2 = 0; //Ending row
+      int c1 = 0; //Starting col
+      int c2 = 0; //Ending col
+
+      //Basic information from the file
+      File f = new File(filename);
+      Scanner s = new Scanner(f);
+      String temp = s.nextLine();
+      String[] info = temp.split(" ");
+      row = Integer.parseInt(info[0]);
+      col = Integer.parseInt(info[1]);
+      timer = Integer.parseInt(info[2]);
+      field = new int[row][col];
+
+      //Fills out the field (If it is open pasture, the value is 0 and if it is
+      //a tree, then the value is -1)
+      for (int r = 0; r < row; r ++){
+        String line = s.nextLine();
+        String[] lineinfo = line.split(" ");
+        for (int c = 0; c < col; c ++){
+          if (line.charAt(c) == '.'){
+            field[r][c] = 0;
+          }
+          else{
+            field[r][c] = -1;
+          }
+        }
       }
-      ans += "\n";
+
+      //The starting and ending positions
+      temp = s.nextLine();
+      info = temp.split(" ");
+      r1 = Integer.parseInt(info[0]) - 1;
+      c1 = Integer.parseInt(info[1]) - 1;
+      r2 = Integer.parseInt(info[2]) - 1;
+      c2 = Integer.parseInt(info[3]) - 1;
+
+      s.close();
+
     }
+    catch (FileNotFoundException e){}
+    return -1;
+  }
+
+  private static int help(int startr, int startc, int endr, int endc, int ans, int timer, int[][] field){
     return ans;
   }
+
   public static void main(String[] args){
-    System.out.println(bronze("makelake.1.in")); //342144
-    System.out.println(bronze("makelake.2.in")); //102762432
-    System.out.println(bronze("makelake.3.in")); //1058992704
-    System.out.println(bronze("makelake.4.in")); //753121152
-    System.out.println(bronze("makelake.5.in")); //1028282688
+    //System.out.println(bronze("makelake.1.in")); //342144
+    //System.out.println(bronze("makelake.2.in")); //102762432
+    //System.out.println(bronze("makelake.3.in")); //1058992704
+    //System.out.println(bronze("makelake.4.in")); //753121152
+    //System.out.println(bronze("makelake.5.in")); //1028282688
+    System.out.println(silver("ctravel.1.in"));
   }
 }
